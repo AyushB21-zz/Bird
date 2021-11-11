88ca1adbcc72c82c682e1e024b30030fb4757536
AyushB21 <ayushbh0612@gmail.com>
2021-11-12 05:00:00 +0530

    Class Pipe Move

---
+++ move2.py
@@ -0,0 +1,12 @@
+def move(self):
+        """
+        move floor so it looks like its scrolling
+        :return: None
+        """
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
