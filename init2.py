1849c58b9b491ceca94f28003fed6de3a9c6b1db
AyushB21 <ayushbh0612@gmail.com>
2021-11-11 07:00:00 +0530

    Class Pipe

---
+++ init2.py
@@ -0,0 +1,20 @@
+def __init__(self, x):
+        """
+        initialize pipe object
+        :param x: int
+        :param y: int
+        :return" None
+        """
+        self.x = x
+        self.height = 0
+
+        # where the top and bottom of the pipe is
+        self.top = 0
+        self.bottom = 0
+
+        self.PIPE_TOP = pygame.transform.flip(pipe_img, False, True)
+        self.PIPE_BOTTOM = pipe_img
+
+        self.passed = False
+
+        self.set_height()
