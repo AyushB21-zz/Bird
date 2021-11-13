83d73dd5ca25eecc213cd9fbaa30c50e5b4e5b4e
AyushB21 <ayushbh0612@gmail.com>
2021-11-13 06:30:00 +0530

    Class Pipe Draw

---
+++ draw2.py
@@ -0,0 +1,8 @@
+def draw(self, win):
+        """
+        Draw the floor. This is two images that move together.
+        :param win: the pygame surface/window
+        :return: None
+        """
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
