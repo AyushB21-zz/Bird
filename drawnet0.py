8a43bfcabde52b974b2d27dd89b17f8f17ebb23b
AyushB21 <ayushbh0612@gmail.com>
2021-11-26 07:40:00 +0530

    Draw net 0

---
+++ drawnet0.py
@@ -0,0 +1,17 @@
+def draw_net(config, genome, view=False, filename=None, node_names=None, show_disabled=True, prune_unused=False,
+             node_colors=None, fmt='svg'):
+    """ Receives a genome and draws a neural network with arbitrary topology. """
+    # Attributes for network nodes.
+    if graphviz is None:
+        warnings.warn("This display is not available due to a missing optional dependency (graphviz)")
+        return
+
+    if node_names is None:
+        node_names = {}
+
+    assert type(node_names) is dict
+
+    if node_colors is None:
+        node_colors = {}
+
+    assert type(node_colors) is dict
