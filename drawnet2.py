33073b004de05a85abe5b3b85208e51ee7b74c38
AyushB21 <ayushbh0612@gmail.com>
2021-11-28 05:10:00 +0530

    Draw net 2

---
+++ drawnet2.py
@@ -0,0 +1,17 @@
+    if prune_unused:
+        connections = set()
+        for cg in genome.connections.values():
+            if cg.enabled or show_disabled:
+                connections.add((cg.in_node_id, cg.out_node_id))
+
+        used_nodes = copy.copy(outputs)
+        pending = copy.copy(outputs)
+        while pending:
+            new_pending = set()
+            for a, b in connections:
+                if b in pending and a not in used_nodes:
+                    new_pending.add(a)
+                    used_nodes.add(a)
+            pending = new_pending
+    else:
+        used_nodes = set(genome.nodes.keys())
